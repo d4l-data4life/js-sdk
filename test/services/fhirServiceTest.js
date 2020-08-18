0f2a6642ba7028dd36b4f0f587b8c92413c61b12
LRueckert <lars.rueckert@data4life.care>
2020-08-18 12:19:54 +0200

    fix bug and add tests for it

--- test/services/fhirServiceTest.js
+++ test/services/fhirServiceTest.js
@@ -79,6 +79,40 @@ describe('prepareSearchParameters', () => {
     });
   });
 
+  it('correctly prepares parameters with exclude_tags', () => {
+    const preparedParams = prepareSearchParameters({
+      tags: ['superhero-origin-story'],
+      exclude_tags: ['superman'],
+    });
+    expect(preparedParams).to.deep.equal({
+      tags: ['superhero-origin-story'],
+      exclude_tags: ['custom=superman'],
+    });
+  });
+
+  it('correctly prepares parameters with exclude_flags', () => {
+    const preparedParams = prepareSearchParameters({
+      tags: ['superhero-origin-story'],
+      exclude_flags: [testVariables.appDataFlag],
+    });
+    expect(preparedParams).to.deep.equal({
+      tags: ['superhero-origin-story'],
+      exclude_tags: [testVariables.appDataFlag],
+    });
+  });
+
+  it('correctly prepares parameters with exclude_tags and exclude_flags', () => {
+    const preparedParams = prepareSearchParameters({
+      tags: ['superhero-origin-story'],
+      exclude_tags: ['superman'],
+      exclude_flags: [testVariables.appDataFlag],
+    });
+    expect(preparedParams).to.deep.equal({
+      tags: ['superhero-origin-story'],
+      exclude_tags: ['custom=superman', testVariables.appDataFlag],
+    });
+  });
+
   it('throws when an unsupported parameter is passed', () => {
     expect(() => prepareSearchParameters({ illegalTag: 'suchIllegalMuchEvil' })).to.throw();
   });
