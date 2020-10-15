7d00cee9493272db6c0e0fec033cbfffe1a7a533
Christian Burtchen <christian.burtchen@gesundheitscloud.de>
2020-10-15 13:23:56 +0200

    prettier

--- test/services/fhirServiceTest.ts
+++ test/services/fhirServiceTest.ts
@@ -20,7 +20,6 @@ import documentRoutes from '../../src/routes/documentRoutes';
 import DocumentReference from '../../src/lib/models/fhir/DocumentReference';
 import { FHIR_VERSION_STU3, FHIR_VERSION_R4 } from '../../src/lib/models/fhir/helper';
 
-
 chai.use(sinonChai);
 
 const { expect } = chai;
