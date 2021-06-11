41869b31c4cda3b0c930f8c4698c1092e413c90b
Jan Huenges <jan.huenges@data4life.care>
2021-06-11 15:42:52 +0200

    ✨ SDK-616 Add support for Device, DeviceUseStatement and Immunization (R4)

---
+++ test/lib/resources/r4/immunization-examples.js
@@ -0,0 +1,13 @@
+import ImmunizationExample from './immunization/immunization-example.json';
+import ImmunizationHistorical from './immunization/immunization-example-historical.json';
+import ImmunizationProtocol from './immunization/immunization-example-protocol.json';
+import ImmunizationRefused from './immunization/immunization-example-refused.json';
+import ImmunizationSubpotent from './immunization/immunization-example-subpotent.json';
+
+export default {
+  ImmunizationExample,
+  ImmunizationHistorical,
+  ImmunizationProtocol,
+  ImmunizationRefused,
+  ImmunizationSubpotent,
+};
